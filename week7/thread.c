36531d37d0dde84f000e4cd086002950681d8815
Cuya <vkcclsrn156@gmail.com>
2026-04-15 10:01:27 +0900

    구조체를 이용해 여러 개의 데이터 사용하기

--- week7/thread.c
+++ week7/thread.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <pthread.h>
 
+//여러개의 데이터 사용 시 구조체 이용
+struct arg_sum {
+    int a;
+    int b;
+    int result;
+};
+
 void* thread_function (void* arg) {
-    int t_num = *(int*)arg;
-    printf("Thread %d is runnning.\n", t_num);
+    struct arg_sum* data = (struct arg_sum*)arg;
+    data->result = data->a + data->b;
+    printf("%d + %d = %d\n", data->a, data->b, data->result);
     return NULL;
 }
+//thread는 메인의 메모리를 참조할 뿐...
 
 int main() {
-    pthread_t tid;
-    int args[10];
+    pthread_t tid[10];
+    struct arg_sum args[10];
 
     for(int i=0;i<10;i++){
-        args[i] = i+1;
-        if(0 != pthread_create(&tid, NULL, thread_function, &args[i])) { //성공 시 0 반환
+        args[i].a = i+1;
+        args[i].b = i+2;
+
+        if(0 != pthread_create(&tid[i], NULL, thread_function, &args[i])) { //성공 시 0 반환
             fprintf(stderr, "pthread_create() failed\n");
             return 1;
         }
     }
 
     //`thread 기다리기
-    pthread_join(tid, NULL);
+    for(int i=0;i<10;i++) {
+        pthread_join(tid[i], NULL); //자동으로 close.
+        printf("Thread return");
+    }
     printf("Thread terminated\n");
 
     return 0;
