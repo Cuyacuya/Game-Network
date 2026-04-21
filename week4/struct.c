dbf10823ab561c4b9cb6b0d395a39c31ca55a60c
cuya <vkcclsrn156@gmail.com>
2026-04-22 00:40:54 +0900

    week4 연습 완

--- week4/struct.c
+++ week4/struct.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h> //c에서 네트워크 주소 변환 함수 사용
 
 int main(){
     struct sockaddr_in server_addr;
@@ -10,7 +10,7 @@ int main(){
     server_addr.sin_family = AF_INET; //IPv4 주소 체계 사용
     server_addr.sin_port = htons(8080); //포트 번호 설정
 
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); //ip -> 이진
     printf("server IP : %s\n", inet_ntoa(server_addr.sin_addr));
     printf("Server Port : %d\n", ntohs(server_addr.sin_port));
 
